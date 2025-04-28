eb483c37035c31fa6bbfb2009f4191650d9b4757
AbhijithCSundaran <csabhi007@gmail.com>
2025-04-28 13:43:55 +0530

    basic structure

---
+++ app/Controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function index(): string
+    {
+        return view('login');
+    }
+	public function dashboard(): string
+    {
+		$template = view('common/header');
+		$template.= view('common/leftmenu');
+		$template.= view('dashboard');
+		$template.= view('common/footer');
+        return $template;
+    }
+}

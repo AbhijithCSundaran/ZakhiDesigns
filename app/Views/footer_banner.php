0fab238513f0a8f940d96af949e3f42f33249684
Bala <balasaravanan.v@v4csolutions.com>
2025-06-08 16:19:07 +0530

    banner refresh section complete

--- app/Views/footer_banner.php
+++ app/Views/footer_banner.php
@@ -1,18 +1,18 @@
-		<!-- <section class="footerbanner" style="background-image:url('<?php echo base_url().ASSET_PATH; ?>assets/images/footerbanner.png')">
-			<div class="container-lg">
-				<div class="row">
-					<div class="col-md-6 leftbox">
-						New<br/>Style
-					</div>
-					<div class="col-md-6 rightbox">
-						<div class="yearblock">2025</div>
-						<button class="btn btn-black">View Collection</button>
-					</div>
-				</div>
-			</div>
-		</section> -->
+<!-- <section class="footerbanner" style="background-image:url('<?php echo base_url() . ASSET_PATH; ?>assets/images/footerbanner.png')">
+            <div class="container-lg">
+                <div class="row">
+                    <div class="col-md-6 leftbox">
+                        New<br/>Style
+                    </div>
+                    <div class="col-md-6 rightbox">
+                        <div class="yearblock">2025</div>
+                        <button class="btn btn-black">View Collection</button>
+                    </div>
+                </div>
+            </div>
+        </section> -->
 
-		<?php
+<?php
 $footerImage = base_url(ASSET_PATH . 'assets/images/footerbanner.png');
 $footerTitle = 'New<br/>Style';
 $footerLink = '#';
@@ -49,24 +49,45 @@ if (!empty($themes['theme_Section3'])) {
 ?>
 
 <?php if ($showFooter): ?>
-<section class="footerbanner" style="background-image:url('<?= $footerImage; ?>')">
-    <div class="container-lg">
-        <div class="row">
-            <div class="col-md-6 leftbox">
-                <?= $footerTitle ?>
-            </div>
-            <div class="col-md-6 rightbox">
-                <div class="yearblock"><?= date('Y') ?></div>
-				<a href="<?= base_url('product/viewcollection') ?>" class="btn btn-black">View Collection</a>
+    <section class="footerbanner" id="footerBanner" style="background-image:url('<?= $footerImage; ?>')">
+        <div class="container-lg">
+            <div class="row">
+                <div class="col-md-6 leftbox" id="footerTitle">
+                    <?= $footerTitle ?>
+                </div>
+                <div class="col-md-6 rightbox">
+                    <div class="yearblock"><?= date('Y') ?></div>
+                    <a href="<?= base_url('product/viewcollection') ?>" class="btn btn-black" id="footerLink">View
+                        Collection</a>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<script>
-// Refresh every 10 minutes
-setTimeout(function () {
-    location.reload();
-}, 600000);
-</script>
-<?php endif; ?>
+    </section>
 
+    <script>
+        const footerBanners = <?= json_encode($validSections); ?>;
+        let footerIndex = 0;
+
+        function updateFooterBanner() {
+            if (!footerBanners.length) return;
+
+            const item = footerBanners[footerIndex];
+
+            // Update background image (no link here)
+            document.getElementById('footerBanner').style.backgroundImage = `url('<?= base_url('public/uploads/themes/') ?>${item.image}')`;
+
+            // Update title text with line breaks
+            document.getElementById('footerTitle').innerHTML = item.name ? item.name.replace(/\n/g, '<br>') : '';
+
+            // The button link stays fixed, so do NOT change href dynamically
+
+            footerIndex = (footerIndex + 1) % footerBanners.length;
+        }
+
+        updateFooterBanner(); // Initial load
+        setInterval(updateFooterBanner, 5000); // Rotate every 5 seconds
+    </script>
+
+
+
+<?php endif; ?>
